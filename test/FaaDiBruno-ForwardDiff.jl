9dfb75e4e8abbc696c85f423dd43129b6da2c8c3
Omar Elrefaei <Elrefaei.Omar@gmail.com>
2023-06-05 15:28:13 -0400

    test that faa_di_bruno works when the # inputs of g ≠ the # outputs of f

--- test/FaaDiBruno-ForwardDiff.jl
+++ test/FaaDiBruno-ForwardDiff.jl
@@ -1,37 +1,40 @@
 using ForwardDiff
 
-resh(x) = reshape(x, size(x, 2), size(x, 1))
-
-# 1st order derivative jacobian(f, x)
-# 2st order derivative jacobian(x->jacobian(f,x), x)
-# This function is a wild hack to take the nth order *while* retaining all the intermediate results
+# 1st order derivative: jacobian(f, x)
+# 2nd order derivative: jacobian(x -> jacobian(f,x), x)
+# This is a wild recursive hack to take the nth order derivative *while* retaining all lower order ones
 function nOrder_ForwardDiff(f, x, order)
     funcs = []
+    neqs = size(f(x), 1)
     push!(funcs, x -> ForwardDiff.jacobian(f, x))
     for i in 2:order
         push!(funcs, x -> ForwardDiff.jacobian(funcs[i - 1], x))
     end
     derivatives = Array{Matrix{Float64}}(undef, order)
     for i in 1:order
-        derivatives[i] = funcs[i](x) |> resh
+        derivatives[i] = reshape(funcs[i](x), neqs, :)
     end
     return derivatives
 end
 
+"""
+Utility function to take the nth order derivative of the composition f(g(x)) using faa_di_bruno.
+First computes the derivatives of the individual Julia functions using ForwardDiff, 
+then calls faa_di_bruno with the jacobian tensors along with preallocated result and workspace.
+"""
 function nOrder_FaaDiBruno(f, g, x, order)
-    # We're working same sized f and g for now
-    @assert size(f(x)) == size(g(x)) && length(g(x)) == length(x)
-    n = size(f(x), 1)
-    dfg = Array{Float64}(undef, n, n^order)
+    nvars = length(x)
+    feqs = size(f(g(x)), 1)
+    dfg = Array{Float64}(undef, feqs, nvars^order)
+    ws = FaaDiBrunoWs(feqs, nvars, order)
     df = nOrder_ForwardDiff(f, g(x), order)
     dg = nOrder_ForwardDiff(g, x, order)
-    ws = FaaDiBrunoWs(n, n, order)
     faa_di_bruno!(dfg, df, dg, order, ws)
 end
 
-f(x) = [x[1]^3 + 3x[2]; -x[2]^3 + 2 * x[3]^2; x[3]^3]
-g(y) = [y[1]^3 + y[2]; y[2]^3 + 2y[2]^2 + 2y[3]; y[3]^3 + 3y[2]]
-x = [2, 3, 7]
+f(x) = [x[1]^3 + 3x[2]; -x[2]^3 * x[3]^2]
+g(y) = [y[1]^3*y[2]; y[3]^5*y[4]^3; y[2]^5] 
+x = [2.0, 3.0, 4.0, 5.0]
 
 # only testing up to 4th order because ForwardDiff is extremely slow with these nested derivatives
 #that slowdown is all compilation time that I strongly suspect is related to all these nested  
