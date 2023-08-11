e1914a0575365a97602654caa8c27d4e40451c4c
Omar Elrefaei <elrefaei.omar@gmail.com>
2023-08-11 18:43:55 -0400

    WIP simulation code

--- src/Simulation.jl
+++ src/Simulation.jl
@@ -1,19 +1,21 @@
+export SimulateWs, simulate
+
 ⊗(a,b) = kron(a,b)
 
-struct sim7
-    y1
-    y2
-    gy
-    gu
-    gσσ
-    gyy
-    gyu
-    guu
-    nstate
-    nshock
-    state_index
+struct SimulateWs
+    y1::Vector{Float64}
+    y2::Vector{Float64}
+    gy::Matrix{Float64}
+    gu::Matrix{Float64}
+    gσσ::Vector{Float64}
+    gyy::Matrix{Float64}
+    gyu::Matrix{Float64}
+    guu::Matrix{Float64}
+    nstate::Int
+    nshock::Int
+    state_index::Vector{Int}
 
-    function sim7(GD, n, solverWs)
+    function SimulateWs(GD, n, solverWs::KOrderWs)
     nstate = solverWs.nstate
     nshock = solverWs.nshock
     state_index = solverWs.state_index
@@ -31,25 +33,28 @@ struct sim7
     gyy = GD[2][ :, vec(K[1:nstate, 1:nstate])]
     gyu = GD[2][ :, vec(K[1:nstate, nstate .+ (1:nshock)])]
     guu = GD[2][ :, vec(K[nstate .+ (1:nshock), nstate .+ (1:nshock)])]
-    new(y1, y2, gy, gu, gσσ, gyy, gyu, guu, nstate, nshock, state_index)
+    new(
+        y1,y2, 
+        gy, gu, gσσ, gyy, gyu, guu, 
+        nstate, nshock, state_index)
     end
 end
 
-SimulateWs = sim7
-
-function simulate_run(GD, t_final, solverWs)
+# FOR DEVELOPMENT: fake single simulation
+function simulate_run(GD, t_final, solverWs::KOrderWs)
     # y0 and ut should be provided by user, but this is some demo inputs
     gy1 = GD[1][:, 1]
     y0 = ones(size(gy1)[1])
-    Main.Random.seed!(0)
+    Main.Random.seed!(0) # just do using Random in repl
     ut = eachcol( randn(solverWs.nshock, t_final).*0.01 )
     
     n = length(y0)
     simWs = SimulateWs(GD, n, solverWs)
-    simulate(GD, y0, ut, t_final, simWs)[:, end]
+    simulate(GD, y0, ut, t_final, simWs)
 end
 
-function simulate(GD, y0, ut, t_final, simWs)
+function simulate(GD, y0, ut, t_final, simWs::SimulateWs)
+    @assert length(ut) == t_final
     n = length(y0)
     # output vector to hold a simulation results
     simulations = Vector{Vector{Float64}}(undef, t_final)
